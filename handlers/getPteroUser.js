634cd981e49c8be88291be763d688bcc2a8638d2
Revolt <118598395+ImRevoltexee@users.noreply.github.com>
2024-06-11 10:30:43 +0700

    Independent

---
+++ handlers/getPteroUser.js
@@ -0,0 +1,22 @@
+const settings = require('../handlers/readSettings').settings(); 
+
+const fetch = require('node-fetch')
+
+if (settings.pterodactyl && settings.pterodactyl.domain && settings.pterodactyl.domain.endsWith("/")) {
+    settings.pterodactyl.domain = settings.pterodactyl.domain.slice(0, -1);
+}
+
+module.exports = async (userid, db) => {
+    return new Promise(async (resolve, err) => {
+        let cacheAccount = await fetch(
+            `${settings.pterodactyl.domain}/api/application/users/${(await db.get(`users-${userid}`))}?include=servers`,
+            {
+                method: "GET",
+                headers: { 'Content-Type': 'application/json', "Authorization": `Bearer ${settings.pterodactyl.key}` }
+            }
+        );
+        if (await cacheAccount.statusText === "Not Found") return err('Ptero account not found');
+        let cacheAccountInfo = JSON.parse(await cacheAccount.text());
+        resolve(cacheAccountInfo);
+    })
+}
